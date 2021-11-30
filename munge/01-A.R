975d4cb8d2e944df4a32f892aa194a8522ad314e
Filippos Pikrides <filipos240498@gmail.com>
2021-11-30 13:09:44 +0000

    removing unecessary code and adding more comments

--- munge/01-A.R
+++ munge/01-A.R
@@ -1,10 +1,11 @@
-# Example preprocessing script.
-#cyber <-security.1_step-activity
-
+##  01-A.R
+## 
+## Author Pikrides Filippos - 210229134
 
 library(dplyr)
 
 ##  2nd set of data 
+##   clean and assign to variables
 data_list2 <- cyber.security.2_enrolments
 enrolment2 <- data.frame(data_list2)
 enrolment2 =na.omit(enrolment2)
@@ -14,6 +15,7 @@ activity2 =na.omit(cyber.security.2.step.activity)
 
 
 ## 3rd set of data 
+##   clean and assign to variables
 enrolment3 <-cyber.security.3_enrolments
 enrolment3 <- data.frame(enrolment3)
 enrolment3 =na.omit(enrolment3)
@@ -21,15 +23,10 @@ enrolment3 <- enrolment3  [(!(enrolment3$gender  =="Unknown") & !(enrolment3$age
  
 activity3 =na.omit(cyber.security.1.step.activity)
 
-## nrow(activity3)
  
-## activity2 <- cyber.security.2.step.activity
-## activity1 <- cyber.security.1.step.activity
-
-## Questionresp4 <- cyber.security.4.question.response
 
 ####  4th set of data 
-
+##   clean and assign to variables
 cyber.security.4.archetype.survey.responses
 leaving4 <- na.omit(cyber.security.4.leaving.survey.responses)
 ## str(cyber.security.4.leaving.survey.responses)
@@ -43,33 +40,13 @@ members4 <- na.omit(cyber.security.4.team.members)
 video4 <- cyber.security.4.video.stats
 responses4 <- cyber.security.4.weekly.sentiment.survey.responses
 
-######################333
+###################### 
+##   merge data 
+## join the enrollments from  set of files  2,3,4
+data23 = bind_rows(enrolment2,enrolment3)
+data = bind_rows(data23,enrolment4)
 
 
-#enrolment1 <- cyber.security.1.enrolments  [(!(cyber.security.1.enrolments$gender  =="Unknown") & !(cyber.security.1.enrolments$age_range  =="Unknown")),]
-#enrolment1 <- data.frame(enrolment1)
-
-#enrolment5 <- cyber.security.5.enrolments  [(!(cyber.security.5.enrolments$gender  =="Unknown") & !(cyber.security.5.enrolments$age_range  =="Unknown")),]
-#enrolment5 <- data.frame(enrolment5)
-
-#enrolment6 <- cyber.security.6.enrolments  [(!(cyber.security.6.enrolments$gender  =="Unknown") & !(cyber.security.6.enrolments$age_range  =="Unknown")),]
-#enrolment6 <- data.frame(enrolment6)
-
-
-
-####  7th set of data 
-
-####  cyber.security.7.archetype.survey.responses
-####  leaving7 <- na.omit(cyber.security.7.leaving.survey.responses)
-## str(cyber.security.4.leaving.survey.responses)
-####  enrolment7 <- na.omit(cyber.security.7.enrolments)
-
-####  enrolment7 <- cyber.security.7.enrolments  [(!(cyber.security.7.enrolments$gender  =="Unknown") & !(cyber.security.7.enrolments$age_range  =="Unknown")),]
-####  enrolment7 <- data.frame(enrolment7)
-####  Questionresp7 <- cyber.security.7.question.response
-####  activity7 <- cyber.security.7.step.activity
-####  members7 <- na.omit(cyber.security.7.team.members)
-####  video7 <- cyber.security.7.video.stats
-####  responses7 <- cyber.security.7.weekly.sentiment.survey.responses
-
-
+## join the activities for set of data 2,3,4
+activity23 =bind_rows(activity2,activity3)
+activity   =bind_rows(activity23,activity4)
